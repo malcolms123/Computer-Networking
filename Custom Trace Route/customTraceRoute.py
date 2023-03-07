5c6afd1fb0c320248d0354fcc801f48f0cc30164
Malcolm Desktop <malcolmsturgis2000@gmail.com>
2023-03-07 14:04:10 -0500

    from desktop

--- Custom Trace Route/customTraceRoute.py
+++ Custom Trace Route/customTraceRoute.py
@@ -53,40 +53,41 @@ def checksum(data):
 	return ~sum & 0xffff
 
 
-def ping(host, skt = None, seqno = 1, ttl = 30, quiet = False):
+def ping(host, skt = None, ttl = 30, quiet = False):
     # setting TTL
     skt.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
     # make initial packet
-    ident = random.randint(0,0xffff)
-    header = struct.pack("!BBHHH",8,0,0,ident,seqno)
-    send_time_ns = int(1e9 * time.time())
-    payload = struct.pack("!Q", send_time_ns)
-    packet = header + payload
+    packet = struct.pack("!BBHHH",8,0,0,0,0)
     # checksum and recreate packet
     c = checksum(packet)
-    header = struct.pack("!BBHHH",8,0,c,ident,seqno)
-    packet = header + payload
+    packet = struct.pack("!BBHHH",8,0,c,0,0)
     # send and receive packet
-    print(f"Pinging {host} with {len(packet)} bytes and ttl = {ttl}.")
+    send_time = time.time()
     skt.sendto(packet, (host,0))
-    resp, addr = skt.recvfrom(1024)
-    rx_time_ns = int(1e9 * time.time())
+    try:
+    	resp, addr = skt.recvfrom(1024)
+    except socket.timeout:
+    	print(f"{ttl}  *  *")
+    	return False
+    rx_time = time.time()
     # decyphering ICMP response
     icmp_type, icmp_code, icmp_checksum, icmp_id, icmp_seq = struct.unpack("!BBHHH",resp[20:28])
+    ip_addr = socket.inet_ntoa(struct.unpack("!4s",resp[12:16])[0])
     # checking for any inconsistencies
-    assert icmp_type == 0 or icmp_type == 11, "Invalid ICMP type"
-    assert icmp_code == 0, "Invalid ICMP code."
-    assert icmp_id == ident or icmp_id == 0, "Invalid ICMP identifier."
-    assert icmp_seq == seqno or icmp_seq == 0, "Invalid ICMP sequence number."
+    assert icmp_type == 0 or icmp_type == 11, f"Invalid ICMP type: {icmp_type}"
+    assert icmp_code == 0, f"Invalid ICMP code: {icmp_code}"
     local_check = checksum(resp)
     assert local_check == 0, "Failed local checksum."
+    # calculating rtt
+    rtt_ms = round(1e3*(rx_time-send_time),4)
+    # printing results
+    print(f"{ttl}  {ip_addr}  {rtt_ms} ms")
+    # testing if final stop
+    if icmp_type == 11:
+    	return False
+    else:
+    	return True
 
-    icmp_send_time = struct.unpack("!Q",resp[28:36])[0]
-    rtt_ns = rx_time_ns - icmp_send_time
-    rtt_ms = rtt_ns/1e6
-
-    print(f"ICMP response: {icmp_id}, {icmp_seq}, [{local_check}], rtt = {rtt_ms} ms")
-    return True, rtt_ms
 
 
 
@@ -95,27 +96,24 @@ def ping(host, skt = None, seqno = 1, ttl = 30, quiet = False):
 if __name__=="__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("host", help="Host to ping")
-	parser.add_argument("-t", "--ttl", help="Time to live", default=30, type=int)
-	parser.add_argument('-n', '--num', help="Number of pings", default=3, type=int)
+	parser.add_argument('--max_hops', help="Maximum hops.", default=30, type=int)
 	args = parser.parse_args()
 	
 	# if doing multiple pings, you can reuse the same socket
 	s = icmp_socket()
-	rtts = []
+	print(f"traceroute to {args.host}, {args.max_hops} hops max")
 	try:				
 		# wrap this in a try block so we can close the socket if there is an error		
-		for i in range(args.num):
-			valid, rtt = ping(args.host, skt = s, seqno = i, ttl=i+1)
-			if valid:	
-				rtts.append(rtt)
+		for i in range(args.max_hops):
+			valid = ping(args.host, skt = s, ttl=i+1)
+			if valid:
+				print(f"{args.host} reached in {i+1} hops.")
+				break
+		if not valid:
+			print(f"{args.host} not reached within {args.max_hops} hops.")
 		
 	except KeyboardInterrupt:
 		print("^C")
 	finally:
 		# close the socket when done
 		s.close()
-
-	print("---  ping statistics ---")
-	print (f"{args.num} packets transmitted, {len(rtts)} packets received, {100*(args.num - len(rtts))/args.num:3.2f}% packet loss, time {sum(rtts):8.3f}ms")
-	if len(rtts) > 0:		
-		print (f"rtt min/avg/max = {min(rtts):6.3f}/{sum(rtts)/len(rtts):6.3f}/{max(rtts):6.3f} ms")
